3d7e9ce284340240e32af4a3557d89656cfb33ce
Marko Stankovic <marefaca2002@gmail.com>
2023-06-21 22:12:01 +0200

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/electro_vue/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue_electro/' : '/',
   transpileDependencies: true
 })
